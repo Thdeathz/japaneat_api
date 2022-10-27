dec8f621ca58afa23ad29e5c793705980e2867db
Thdeathz <buidunga112@gmail.com>
2022-10-28 02:13:51 +0700

    fix: fix deploy

--- database/seeders/AchievementSeeder.php
+++ database/seeders/AchievementSeeder.php
@@ -17,7 +17,7 @@ class AchievementSeeder extends Seeder
         DB::table('achievements')->insert([
             [
                 'name' => 'Lofi music 1',
-                'image' => 'https://firebasestorage.googleapis.com/v0/b/japaneat-525ab.appspot.com/o/images%2Ft%E1%BA%A3i%20xu%E1%BB%91ng.png?alt=media&token=7a499c59-e821-45ab-93da-909c7b126b68',
+                'image' => 'https://firebasestorage.googleapis.com/v0/b/japaneat-525ab.appspot.com/o/images%2F83446803_p0_master1200.png?alt=media&token=4fc03332-cf9b-4685-a868-f1616665da15',
                 'url' => 'https://firebasestorage.googleapis.com/v0/b/japaneat-525ab.appspot.com/o/audio%2FChill%20Work%20Music%20%E2%80%94%20Calm%20Focus%20Mix.mp3?alt=media&token=b6951ff2-1685-4420-8474-bfb05a6b6165',
                 'require_point' => 100,
                 'type' => 0
@@ -25,7 +25,7 @@ class AchievementSeeder extends Seeder
             [
                 'name' => 'Lofi music 2',
                 'url' => 'https://firebasestorage.googleapis.com/v0/b/japaneat-525ab.appspot.com/o/audio%2FNh%E1%BB%AFng%20B%C3%A0i%20H%C3%A1t%20Lofi%20Acoustic%20Ti%E1%BA%BFng%20Anh%20C%E1%BB%B1c%20Chill%20Hay%20Nh%E1%BA%A5t%20-%20Nh%E1%BA%A1c%20Lofi%20Chill%20Tik%20Tok%20Nh%E1%BA%B9%20Nh%C3%A0ng.mp3?alt=media&token=92bc2078-e0bc-4583-8f05-0822c6ce4977',
-                'image' => 'https://firebasestorage.googleapis.com/v0/b/japaneat-525ab.appspot.com/o/images%2Ft%E1%BA%A3i%20xu%E1%BB%91ng.png?alt=media&token=7a499c59-e821-45ab-93da-909c7b126b68',
+                'image' => 'https://firebasestorage.googleapis.com/v0/b/japaneat-525ab.appspot.com/o/images%2F83446803_p0_master1200.png?alt=media&token=4fc03332-cf9b-4685-a868-f1616665da15',
                 'require_point' => 100,
                 'type' => 0
             ],
